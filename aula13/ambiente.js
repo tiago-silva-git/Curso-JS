0113d0cb9be37f4f3a1d193f9f1e6891a04800ff
tiago-silva-git <tiago.silva.qa@gmail.com>
2021-03-11 20:46:58 -0300

    Aprendendo loop while

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 500) {
+    console.log(`Tudo bem? ${c} vezes.`)
+    c += 1
+}
+*/
+
+var c = 1
+do { 
+    console.log(`Tudo bem? ${c} vezes.`)
+    c += 1
+} while (c <= 10) 
